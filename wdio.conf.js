592039e03f3deedff7e199b32a9f6e9e245fc8dc
Oseias Nascimento <oseiasal@gmail.com>
2022-07-25 10:03:26 -0300

    device farm instalado

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,10 +1,16 @@
 const { join } = require('path')
 const allure = require('allure-commandline')
+const video = require('wdio-video-reporter');
 
 exports.config = {
-    hostname: 'localhost',
-    port: 3005,
-    path: '/wd/hub',
+    // hostname: 'localhost',
+    // port: 3005,
+    // path: '/wd/hub',
+    user: 'osiasnascimento_kT7VCy',
+    key: 'GyemdjZx9dL7V86sdQwq',
+    services: [
+        ['browserstack']
+    ],
     specs: [
         './test/specs/**/*.spec.js'
     ],
@@ -14,17 +20,27 @@ exports.config = {
         timeout: 40000
     },
     capabilities: [{
-        "platformName": "Android",
-        "platformVersion": "8.1",
-        "deviceName": "emulator-5558",
-        "automationName": "UiAutomator2",
-        "app": join(process.cwd(), "./app/android/loja-ebac.apk"),
-        "appWaitActivity": "com.woocommerce.android.ui.login.LoginActivity",
-        "appWaitPackage": "com.woocommerce.android",
+        'app': 'bs://80c016dd7f5b4c35b59b5cb4b37f76c39bb0623a',
+        'device': 'Google Pixel 3',
+        'os_version': '9.0',
+        'project': 'Primeiro projeto em device farm',
+        'build': '2',
+        'name': 'first_test'
+
+        // "platformName": "Android",
+        // "platformVersion": "8.1",
+        // "deviceName": "emulator-5558",
+        // "automationName": "UiAutomator2",
+        // "app": join(process.cwd(), "./app/android/loja-ebac.apk"),
+        // "appWaitActivity": "com.woocommerce.android.ui.login.LoginActivity",
+        // "appWaitPackage": "com.woocommerce.android",
     }], reporters: ['spec', ['allure', {
         outputDir: 'allure-results',
         disableWebdriverStepsReporting: false,
         disableWebdriverScreenshotsReporting: false,
+    }], [video, {
+        saveAllVideos: false,       // If true, also saves videos for successful test cases
+        videoSlowdownMultiplier: 50, // Higher to get slower videos, lower for faster videos [Value 1-100]
     }]],
     onComplete: function () {
         const reportError = new Error('Could not generate Allure report')
